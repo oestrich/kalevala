9940c274662e8d94578ad0a1279a1fb16b0b427e
Eric Oestrich <eric@oestrich.org>
2020-02-08 12:50:22 -0500

    Clean up the kalevala application

--- lib/kalevala/application.ex
+++ lib/kalevala/application.ex
@@ -1,18 +1,10 @@
 defmodule Kalevala.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: Kalevala.Worker.start_link(arg)
-      # {Kalevala.Worker, arg}
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+    children = []
     opts = [strategy: :one_for_one, name: Kalevala.Supervisor]
     Supervisor.start_link(children, opts)
   end
