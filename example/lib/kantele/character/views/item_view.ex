68b03d892b1f7f0e825ba4a2a69e960b095969e7
Eric Oestrich <eric@oestrich.org>
2020-04-28 15:30:23 -0400

    Render the item name once and reuse it
    Same with the unknown text

--- example/lib/kantele/character/views/item_view.ex
+++ example/lib/kantele/character/views/item_view.ex
@@ -3,20 +3,24 @@ defmodule Kantele.Character.ItemView do
 
   import IO.ANSI, only: [reset: 0, white: 0]
 
+  def render("name", %{item: item}) do
+    ~i(#{white()}#{item.name}#{reset()})
+  end
+
   def render("drop-abort", %{reason: :no_item, item_name: item_name}) do
-    ~i(There is no item #{white()}"#{item_name}"#{reset()}.\n)
+    render("unknown", %{item_name: item_name})
   end
 
   def render("drop-commit", %{item: item}) do
-    ~i(You dropped #{white()}#{item.name}#{reset()}.\n)
+    ~i(You dropped #{render("name", %{item: item})}.\n)
   end
 
   def render("pickup-abort", %{reason: :no_item, item_name: item_name}) do
-    ~i(There is no item #{white()}"#{item_name}"#{reset()}.\n)
+    render("unknown", %{item_name: item_name})
   end
 
   def render("pickup-commit", %{item: item}) do
-    ~i(You picked up #{white()}#{item.name}#{reset()}.\n)
+    ~i(You picked up #{render("name", %{item: item})}.\n)
   end
 
   def render("unknown", %{item_name: item_name}) do
