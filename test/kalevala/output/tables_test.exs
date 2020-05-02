c13c5a63a76c53c441fde4bb1a38d04a8222dd1a
Eric Oestrich <eric@oestrich.org>
2020-05-02 15:31:33 -0400

    Move table parsing tests into the table test file

--- test/kalevala/output/tables_test.exs
+++ test/kalevala/output/tables_test.exs
@@ -1,7 +1,95 @@
 defmodule Kalevala.Output.TablesTest do
   use ExUnit.Case
 
+  alias Kalevala.Output
+  alias Kalevala.Output.Tags
+  alias Kalevala.Output.TagColors
   alias Kalevala.Output.Tables
+
+  describe "parsing tables" do
+    test "parses table tags" do
+      iodata = [~s(Table {table}   {row}\n{cell}Hello, world{/cell}{/row}{/table} Table\n)]
+
+      actual_text =
+        iodata
+        |> Output.process(Tags)
+        |> Output.process(Tables)
+        |> Enum.join("")
+
+      expected_text = """
+      Table 
+      +--------------+
+      | Hello, world |
+      +--------------+
+       Table
+      """
+
+      assert actual_text == expected_text
+    end
+
+    test "complicated table" do
+      iodata = """
+      {table}
+        {row}
+          {cell}Player Name{/cell}
+        {/row}
+        {row}
+          {cell}HP{/cell}
+          {cell}{color foreground="red"}50/50{/color}{/cell}
+        {/row}
+        {row}
+          {cell}Hello{/cell}
+          {cell}Hiya{/cell}
+          {cell}Howdy{/cell}
+        {/row}
+      {/table}
+      """
+
+      actual_text =
+        iodata
+        |> Output.process(Tags)
+        |> Output.process(Tables)
+        |> Output.process(TagColors)
+        |> Enum.join("")
+
+      expected_text = """
+      +----------------------+
+      |     Player Name      |
+      +----------------------+
+      |   HP    |   \e[31m50/50\e[0m    |
+      +----------------------+
+      | Hello | Hiya | Howdy |
+      +----------------------+
+      """
+
+      assert actual_text == expected_text
+    end
+
+    test "extra text outside of a cell skips processing" do
+      iodata = [~s(Table {table}{row}Hello, {cell}world{/cell}{/row}{/table} Table)]
+
+      actual_text =
+        iodata
+        |> Output.process(Tags)
+        |> Output.process(Tables)
+
+      assert actual_text == [
+               "Table ",
+               {:open, "table", %{}},
+               "",
+               {:open, "row", %{}},
+               "Hello, ",
+               {:open, "cell", %{}},
+               "world",
+               {:close, "cell"},
+               "",
+               {:close, "row"},
+               "",
+               {:close, "table"},
+               " Table"
+             ]
+    end
+  end
 
   describe "table tag breathing room" do
     test "text before with no new line" do
--- test/kalevala/output_test.exs
+++ test/kalevala/output_test.exs
@@ -2,7 +2,6 @@ defmodule Kalevala.Output.TagsTest do
   use ExUnit.Case
 
   alias Kalevala.Output
-  alias Kalevala.Output.Tables
   alias Kalevala.Output.Tags
   alias Kalevala.Output.TagColors
 
@@ -60,91 +59,6 @@ defmodule Kalevala.Output.TagsTest do
         |> Output.process(TagColors)
 
       assert text == [~s(hello, {color foreground="white"}world{/color})]
-    end
-  end
-
-  describe "tables" do
-    test "parses table tags" do
-      iodata = [~s(Table {table}   {row}\n{cell}Hello, world{/cell}{/row}{/table} Table\n)]
-
-      actual_text =
-        iodata
-        |> Output.process(Tags)
-        |> Output.process(Tables)
-        |> Enum.join("")
-
-      expected_text = """
-      Table 
-      +--------------+
-      | Hello, world |
-      +--------------+
-       Table
-      """
-
-      assert actual_text == expected_text
-    end
-
-    test "complicated table" do
-      iodata = """
-      {table}
-        {row}
-          {cell}Player Name{/cell}
-        {/row}
-        {row}
-          {cell}HP{/cell}
-          {cell}{color foreground="red"}50/50{/color}{/cell}
-        {/row}
-        {row}
-          {cell}Hello{/cell}
-          {cell}Hiya{/cell}
-          {cell}Howdy{/cell}
-        {/row}
-      {/table}
-      """
-
-      actual_text =
-        iodata
-        |> Output.process(Tags)
-        |> Output.process(Tables)
-        |> Output.process(TagColors)
-        |> Enum.join("")
-
-      expected_text = """
-      +----------------------+
-      |     Player Name      |
-      +----------------------+
-      |   HP    |   \e[31m50/50\e[0m    |
-      +----------------------+
-      | Hello | Hiya | Howdy |
-      +----------------------+
-      """
-
-      assert actual_text == expected_text
-    end
-
-    test "extra text outside of a cell skips processing" do
-      iodata = [~s(Table {table}{row}Hello, {cell}world{/cell}{/row}{/table} Table)]
-
-      actual_text =
-        iodata
-        |> Output.process(Tags)
-        |> Output.process(Tables)
-
-      assert actual_text == [
-               "Table ",
-               {:open, "table", %{}},
-               "",
-               {:open, "row", %{}},
-               "Hello, ",
-               {:open, "cell", %{}},
-               "world",
-               {:close, "cell"},
-               "",
-               {:close, "row"},
-               "",
-               {:close, "table"},
-               " Table"
-             ]
     end
   end
 end
