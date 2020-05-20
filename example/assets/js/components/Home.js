6270425cc7b9aabd712a52102abe7dc8a9927e14
Eric Oestrich <eric@oestrich.org>
2020-05-20 17:31:32 -0400

    Better mobile view for the home page

--- example/assets/js/components/Home.js
+++ example/assets/js/components/Home.js
@@ -5,36 +5,34 @@ import { Link } from "react-router-dom";
 import { getLoginActive } from "../redux";
 
 class Home extends React.Component {
-  render() {
-    const renderLogin = () => {
-      if (this.props.loginActive) {
-        return (<Link className="btn-primary" to="/login">Login</Link>);
-      } else {
-        return (<a className="btn-inactive">Login</a>);
-      }
-    };
+  renderLogin() {
+    if (this.props.loginActive) {
+      return (<Link className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white" to="/login">Login</Link>);
+    } else {
+      return (<a className="inline-block text-sm px-4 py-2 leading-none border rounded text-gray-200 border-gray-200 hover:border-transparent hover:text-gray-500 hover:bg-white">Login</a>);
+    }
+  }
 
+  render() {
     return (
-      <div>
-        <nav className="bg-teal-600 p-2 mt-0 fixed w-full z-10 top-0">
+      <div className="bg-gray-200">
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-4">
           <div className="container mx-auto flex flex-wrap items-center">
-            <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
-              <Link to="/" className="text-white no-underline hover:text-white hover:no-underline">
-                <span className="text-2xl pl-2">Kantele</span>
+            <div className="flex items-center flex-shrink-0 text-white w-1/2">
+              <Link to="/" className="text-white no-underline hover:text-white hover:no-underline pl-2">
+                <span className="font-semibold text-xl tracking-tight">Kantele</span>
               </Link>
             </div>
 
-            <div className="flex w-full content-center justify-between md:w-1/2 md:justify-end">
-              <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
-                <li className="mr-3">{renderLogin()}</li>
-              </ul>
+            <div className="flex items-center content-center w-1/2 justify-end">
+              {this.renderLogin()}
             </div>
           </div>
         </nav>
 
-        <div className="container mx-auto mt-24 md:mt-16 h-screen">
-          <div className="bg-gray-200 p-8 rounded">
-            <p className="text-4xl">Kantele is a text multiplayer adventure game.</p>
+        <div className="container mx-auto mt-4 px-4 sm:px-0 h-screen">
+          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <p className="text-xl sm:text-4xl">Kantele is a multiplayer text adventure.</p>
             <p>This is the sample game for <a className="underline text-gray-700" href="https://github.com/oestrich/kalevala" target="_blank">Kalevala</a>, a world building toolkit written in Elixir.</p>
           </div>
         </div>
